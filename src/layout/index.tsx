9b327a3cf48b1ab15fc79870133b5be2d7d84e2c
ALiuYiLin <scliuyilin@163.com>
2026-01-22 17:54:50 +0800

    feat: 添加侧边栏导航菜单

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,26 @@
-import { Flex } from 'antd'
-import { Outlet } from 'react-router-dom'
-import { UserStats } from '@/components/UserStats'
+import { Flex, Menu } from 'antd'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
+import { UserOutlined, ContactsOutlined } from '@ant-design/icons'
 
 export const Layout = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const menuItems = [
+    { key: '/', icon: <UserOutlined />, label: '用户管理' },
+    { key: '/lead', icon: <ContactsOutlined />, label: '线索管理' },
+  ]
+
   return (
     <Flex className="h-screen w-screen">
-      <Flex className="w-75 h-full bg-gray-50 border-r">
-        <UserStats />
+      <Flex vertical className="w-50 h-full bg-gray-50 border-r">
+        <Menu
+          mode="inline"
+          selectedKeys={[location.pathname]}
+          items={menuItems}
+          onClick={({ key }) => navigate(key)}
+          style={{ border: 'none', background: 'transparent' }}
+        />
       </Flex>
       <Outlet />
     </Flex>
